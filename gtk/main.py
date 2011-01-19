e7751eb78093524bf19fcf13ed2867b0959ecedc
Guy Taylor <thebigguy.co.uk@gmail.com>
2011-01-19 20:00:26 +0000

    refactor all the gstreamer/video stuff into a new GTK widget

--- gtk/main.py
+++ gtk/main.py
@@ -2,12 +2,9 @@
 
 import sys, os
 import pygtk, gtk, gobject
-import pygst
-pygst.require("0.10")
-import gst
 import vte
-import cairo
 
+from gst_widget import GstDrawingArea
 
 from time import time
 
@@ -17,7 +14,6 @@ class GTK_Main(object):
     # save init values
     self.debug=debug
     self.fullscreen = False # this is technicaly not consistant as it is not chnaged on system uests
-    self.pipeline = None
     
     # setup the window
     self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
@@ -79,10 +75,9 @@ class GTK_Main(object):
       button = gtk.Button("test "+str(i))
       vbox_leftpanel.add(button)
     
-    self.movie_window = gtk.DrawingArea()
-    self.movie_window.set_size_request(500, 400)
-    self.movie_window.connect('expose_event', self.expose_moviewindow)
-    hbox_rightpanel_top.add(self.movie_window)
+    # TODO
+    self.gst = GstDrawingArea()
+    hbox_rightpanel_top.add(self.gst)
     
     self.vte = vte.Terminal()
     self.vte.connect ("child-exited", self.respawn_vte)
@@ -90,32 +85,12 @@ class GTK_Main(object):
     hbox_rightpanel_bottom.add(self.vte)
     
     self.window.show_all()
-  
-  def expose_moviewindow(self, widget=None, data=None):
-    if self.pipeline != None:
-      return
-    drawable = self.movie_window.window
-    pixbuf = gtk.gdk.pixbuf_new_from_file("logo.png")
-    ctx = drawable.cairo_create()
-    ctx.set_source_pixbuf(pixbuf,0,0)
-    x, y = drawable.get_size()
-    x = 500
-    y = 400
-    ctx.scale(x, y)
-    ctx.paint()
-    ctx.stroke()
 
   def respawn_vte(self, widget):
     self.vte.fork_command()
   
   def save_frame(self, widget=None, data=None):
-    drawable = self.movie_window.window
-    colormap = drawable.get_colormap()
-    pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, 0, 8, *drawable.get_size())
-    pixbuf = pixbuf.get_from_drawable(drawable, colormap, 0,0,0,0, *drawable.get_size())
-    name = "frame_" + str( time() ).replace(".", "")
-    pixbuf.save(name + ".png", 'png')
-    pixbuf.save(name + ".jpeg", 'jpeg')
+    self.gst.save_frame(widget=widget, data=data)
 
   
   def on_key_press(self, widget, data=None):
@@ -154,88 +129,21 @@ class GTK_Main(object):
     gtk.main_quit()
     
   def start_stop(self, widget, data=None):
-    if self.pipeline == None:
-      self.start_feed()
+    if self.gst.is_playing():
+      self.stop_feed()
     else:
-      self.stop_feed()
-            
-  def gst_on_message(self, bus, message):
-    t = message.type
-    if t == gst.MESSAGE_EOS: # end of feed
-      self.stop_feed()
-    elif t == gst.MESSAGE_ERROR:
-      err, debug = message.parse_error()
-      print "Error: %s" % err, debug
-      self.stop_feed()
-  
-  def gst_on_sync_message(self, bus, message):
-    if message.structure is None:
-      return
-    message_name = message.structure.get_name()
-    if message_name == "prepare-xwindow-id":
-      imagesink = message.src
-      imagesink.set_property("force-aspect-ratio", True)
-      gtk.gdk.threads_enter()
-      imagesink.set_xwindow_id(self.movie_window.window.xid)
-      gtk.gdk.threads_leave()
-  
+      self.start_feed()
+    
   def start_feed(self):
-    self.build_pipeline()
-    self.pipeline.set_state(gst.STATE_PLAYING)
+    fixcolour = self.button_fixcolour.get_active()
+    self.gst.start_video(self.feed_radio, fixcolour=fixcolour)
     self.button.set_label("Stop")
     self.button.set_active(True)
   
   def stop_feed(self):
-    self.pipeline.set_state(gst.STATE_NULL)
-    self.pipeline = None
+    self.gst.stop_video()
     self.button.set_label("Start Feed")
     self.button.set_active(False)
-    self.expose_moviewindow()
-  
-  def build_pipeline(self, rotation="clockwise", text="%feed_type%"):
-    
-    # pare the text
-    if self.feed_radio == "test":
-      text = text.replace("%feed_type%", "Test Feed")
-    else:
-      text = text.replace("%feed_type%", "Real Feed")
-    
-    # make the gstreamer pipline
-    self.pipeline = gst.Pipeline("webcam")
-    
-    if self.feed_radio == "test":
-      source = gst.element_factory_make("videotestsrc", "webcam-source")
-    else:
-      source = gst.element_factory_make("v4l2src", "webcam-source")
-    
-    # make the pipline elements
-    videorotate = gst.element_factory_make("videoflip", "rotate video")
-    videorotate.set_property("method", rotation)
-    
-    textoverlay = gst.element_factory_make("textoverlay", "textoverlay")
-    textoverlay.set_property("text", text)
-    textoverlay.set_property("valign", "top")
-    textoverlay.set_property("halign", "right")
-    textoverlay.set_property("shaded-background", "yes")
-    textoverlay.set_property("font-desc", "Sans Bold 18")
-    
-    videosink = gst.element_factory_make("autovideosink", "video-output")
-    
-    # add the elemnts to the pipline
-    if self.button_fixcolour.get_active():
-      colorspace = gst.element_factory_make("ffmpegcolorspace", "colorspace")
-      self.pipeline.add(source, colorspace, videorotate, textoverlay, videosink)
-      gst.element_link_many(source, colorspace, videorotate, textoverlay, videosink)
-    else:
-      self.pipeline.add(source, videorotate, textoverlay, videosink)
-      gst.element_link_many(source, videorotate, textoverlay, videosink)
-    
-    # get accses to the pipline message bus
-    bus = self.pipeline.get_bus()
-    bus.add_signal_watch()
-    bus.enable_sync_message_emission()
-    bus.connect("message", self.gst_on_message)
-    bus.connect("sync-message::element", self.gst_on_sync_message)
   
   def radio_feed_change(self, widget, data=None):
     if self.feed_radio == "real":
